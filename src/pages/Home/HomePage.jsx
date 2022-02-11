005bd47a6f7a8cd3b47b5d89ef6757aced21e898
IshtiaqueNafis <nafu22@gmail.com>
2022-02-11 11:53:42 -0500

    created page components

---
+++ src/pages/Home/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './home.module.css';
+
+const HomePage = () => {
+    return (
+        <div>
+            Home
+        </div>
+    );
+};
+
+export default HomePage;
